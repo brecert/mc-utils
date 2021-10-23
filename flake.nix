856ac18852b5f4fef343d2259d06f5133fdc5a6c
brecert <me@bree.dev>
2021-10-23 05:27:37 -0400

    add nix support

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakeCompat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nixCargoIntegration = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.nixCargoIntegration.lib.makeOutputs {
+      root = ./.;
+      defaultOutputs = {
+        app = "mc-utils";
+        package = "mc-utils";
+      };
+    };
+}
